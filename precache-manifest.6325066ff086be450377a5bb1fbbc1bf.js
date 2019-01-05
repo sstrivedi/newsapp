601d2e526336546c69f70957ddc5245627defd61
S Trivedi <shaptrivedi@gmail.com>
2019-01-04 22:45:55 -0500

    Updates

---
+++ precache-manifest.6325066ff086be450377a5bb1fbbc1bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/newsapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "1609f9d65b7ae16a8fbc",
+    "url": "/newsapp/static/css/main.74c2ed07.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/newsapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e0d0382314c6fefe9ef7",
+    "url": "/newsapp/static/js/1.e0d03823.chunk.js"
+  },
+  {
+    "revision": "a68a04643953e8e2c8a9",
+    "url": "/newsapp/static/js/runtime~main.a68a0464.js"
+  },
+  {
+    "revision": "1609f9d65b7ae16a8fbc",
+    "url": "/newsapp/static/js/main.1609f9d6.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/newsapp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/newsapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/newsapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "e0d0382314c6fefe9ef7",
+    "url": "/newsapp/static/css/1.14e8acbd.chunk.css"
+  },
+  {
+    "revision": "5612415a92f31bb12aa0bd0fb761c89b",
+    "url": "/newsapp/index.html"
+  }
+];
